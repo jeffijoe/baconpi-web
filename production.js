5534e5c50cbdf919a807a7cdfca19445a06a3d6c
Jeff Hansen <jeffijoe@hotmail.com>
2014-06-21 12:41:57 +0200

    Added Forever to the mix.

---
+++ production.js
@@ -0,0 +1,14 @@
+'use strict';
+var forever = require('forever-monitor');
+
+var child = new(forever.Monitor)('app.js', {
+  max: 3,
+  silent: true,
+  options: []
+});
+
+child.on('exit', function() {
+  console.log('App has exited after 3 restarts');
+});
+
+child.start();
